dd9eef9eef9250f338c4187203add70649e5b9c4
AlbertoCorcheroGarcia <albertocorcherodaw@gmail.com>
2021-05-05 15:50:25 +0000

    new

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,17 +1,15 @@
 import React from "react";
 import App from "./modal.js";
 import Board from "./board.js";
-import Fill from "./fill,jsx";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+//create your first component <Fill />
 export function Home() {
 	return (
 		<div className="text-center mt-5">
 			<App />
 			<Board />
-			<Fill />
 		</div>
 	);
 }
--- src/js/component/printvalue.js
+++ src/js/component/printvalue.js
@@ -1,22 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 const PrintValue = props => {
 	const [value, setValue] = useState("polla");
 	let print = "";
-
 	//let check = 0;
-	if (props.value == true /*&& check == 0*/) {
+	if (value == true /*&& check == 0*/) {
 		print = "X";
-		console.log(print);
 		//check = check + 1;
 	}
 	if (value == false /*&& check == 0*/) {
 		print = "O";
-		console.log(print);
 		//check = check + 1;
 	}
 	return (
-		<div className="value" onClick={props.onClick}>
+		<div className="value" onClick={() => setValue(props.value)}>
 			{print}
 		</div>
 	);
@@ -24,6 +21,5 @@ const PrintValue = props => {
 export default PrintValue;
 
 PrintValue.propTypes = {
-	value: PropTypes.bool,
-	onClick: PropTypes.func
+	value: PropTypes.bool
 };
